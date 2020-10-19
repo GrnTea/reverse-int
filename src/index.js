25d82c4fb064fc69b387b063c2b4820a639597d7
Ekaterina Matveyeva <grinteabox@gmail.com>
2020-10-19 13:18:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  let arr =  n.toString().split('');
   
+  if (arr.indexOf('-') === 0) {
+    arr.splice(0, 1);
+  };
+  
+  if (arr.lastIndexOf('0') === arr.length - 1) {
+    arr.splice(arr.length - 1, 1);
+  }
+
+  return +arr.reverse().join('');
 }
